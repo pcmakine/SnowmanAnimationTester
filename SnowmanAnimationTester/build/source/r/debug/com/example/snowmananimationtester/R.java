fe3f04085b99945b4eaa130caf575c33f6a23430
Petri Makinen <petri.mkinen1@gmail.com>
2013-08-27 11:07:00 +0300

    surfacegameview has a walking snowman plus you can set the number in a textview by dragging your finger accross the screen

--- SnowmanAnimationTester/build/source/r/debug/com/example/snowmananimationtester/R.java
+++ SnowmanAnimationTester/build/source/r/debug/com/example/snowmananimationtester/R.java
@@ -10,41 +10,54 @@ package com.example.snowmananimationtester;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        /**  From: file:/C:/Users/Pete/AndroidStudioProjects/SnowmanAnimationTesterProject/SnowmanAnimationTester/src/main/res/values/colors.xml 
+         */
+        public static final int black_overlay=0x7f040000;
+    }
     public static final class dimen {
         /**  From: file:/C:/Users/Pete/AndroidStudioProjects/SnowmanAnimationTesterProject/SnowmanAnimationTester/src/main/res/values/dimens.xml 
  From: file:/C:/Users/Pete/AndroidStudioProjects/SnowmanAnimationTesterProject/SnowmanAnimationTester/src/main/res/values-sw720dp-land/dimens.xml 
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int walkingsnowmanspritesheet=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int button=0x7f080001;
-        public static final int court=0x7f080000;
-        public static final int frames=0x7f080004;
-        public static final int hello=0x7f080002;
-        public static final int textView=0x7f080005;
-        public static final int time=0x7f080003;
+        public static final int action_settings=0x7f09000b;
+        public static final int button=0x7f090001;
+        public static final int court=0x7f090000;
+        public static final int energyPlayed=0x7f09000a;
+        public static final int frames=0x7f090004;
+        public static final int gameview=0x7f090007;
+        public static final int hello=0x7f090002;
+        public static final int hit_button=0x7f090008;
+        public static final int seekBar=0x7f090009;
+        public static final int startGameButton=0x7f090006;
+        public static final int textView=0x7f090005;
+        public static final int time=0x7f090003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_menu=0x7f030001;
+        public static final int activity_surfacegame=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int menu=0x7f070001;
+        public static final int main=0x7f080000;
+        public static final int menu=0x7f080001;
+        public static final int surface_game=0x7f080002;
     }
     public static final class string {
         /**  From: file:/C:/Users/Pete/AndroidStudioProjects/SnowmanAnimationTesterProject/SnowmanAnimationTester/src/main/res/values/strings.xml 
          */
-        public static final int action_settings=0x7f050000;
-        public static final int app_name=0x7f050001;
-        public static final int hello_world=0x7f050002;
-        public static final int title_activity_menu=0x7f050003;
+        public static final int action_settings=0x7f060000;
+        public static final int app_name=0x7f060001;
+        public static final int hello_world=0x7f060002;
+        public static final int title_activity_menu=0x7f060003;
+        public static final int title_activity_surface_game=0x7f060004;
     }
     public static final class style {
         /**  From: file:/C:/Users/Pete/AndroidStudioProjects/SnowmanAnimationTesterProject/SnowmanAnimationTester/src/main/res/values/styles.xml 
@@ -54,9 +67,9 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
